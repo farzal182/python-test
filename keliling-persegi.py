e6a2ff347344be903c4a2ae2df6ae0eff1c87f9e
Fakhrizal <farzalanan_ini@yahoo.co.id>
2019-10-15 12:11:36 +0700

    Create keliling-persegi.py

---
+++ keliling-persegi.py
@@ -0,0 +1,16 @@
+print "Program Sederhana"
+print "================="
+
+panjang = int(input("1. Berapakah panjangnya: "))
+
+lebar = int(input("2. Berapakah lebarnya: "))
+
+keliling = 2*(panjang*lebar)
+luas = panjang*lebar
+
+print " "
+print "Hasil Perhitungan"
+print "================="
+
+print "* Kelilingnya adalah ", keliling;
+print "* Luasnya adalah ", luas;
